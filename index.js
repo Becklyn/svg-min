9493e841374670e6d7eb8a9d51edf7f642bf23a1
Jannik Zschiesche <jz@becklyn.com>
2017-10-30 18:01:23 +0100

    Add option to overwrite input files

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const argv = require("minimist")(process.argv.slice(2));
 const Minifier = require("./lib/Minifier");
 
-const minifier = new Minifier(argv._[0], argv._[1]);
+
+const minifier = new Minifier(argv._[0], argv._[1], argv.keep === true);
 minifier.run();
