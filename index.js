0078fa789e1595033dbb0b7cd613cb20505c45d6
Jannik Zschiesche <jz@becklyn.com>
2017-10-30 18:21:26 +0100

    Add verbose option to runner

--- index.js
+++ index.js
@@ -2,5 +2,11 @@ const argv = require("minimist")(process.argv.slice(2));
 const Minifier = require("./lib/Minifier");
 
 
-const minifier = new Minifier(argv._[0], argv._[1], argv.keep === true);
+const minifier = new Minifier(
+    argv._[0],
+    argv._[1], {
+        keepOriginal: argv.keep === true,
+        verbose: argv.v === true,
+    }
+);
 minifier.run();
