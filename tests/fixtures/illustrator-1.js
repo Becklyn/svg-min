3a74ebb69e6d4882dbf18d1956120e9e8aad413a
Jannik Zschiesche <jz@becklyn.com>
2017-11-07 15:37:36 +0100

    Add test cases

---
+++ tests/fixtures/illustrator-1.js
@@ -0,0 +1,15 @@
+module.exports = {
+    message: "Example output file from Illustrator.",
+    in: `
+<?xml version="1.0" encoding="utf-8"?>
+<!-- Generator: Adobe Illustrator 18.1.1, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 width="16.1px" height="8.8px" viewBox="-1498.6 2496.5 16.1 8.8" enable-background="new -1498.6 2496.5 16.1 8.8"
+	 xml:space="preserve">
+<polyline fill="none" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" points="
+	-1483.2,2497.2 -1490.5,2504.6 -1497.8,2497.2 "/>
+</svg>
+`,
+    out: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="-1499.4 2495.7 16.1 8.8"><path fill="none" stroke="#fff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="M-1484 2496.5l-7.3 7.4-7.3-7.4"/></svg>`,
+};
